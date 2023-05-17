765dc7eb6cd161eb2dd9656fec83f98e6a93d272
TechWizard <133467760+zhiyuehh@users.noreply.github.com>
2023-05-17 16:42:56 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,9 +18,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'zhiyuehh', // Usually your GitHub org/user name.
+  projectName: 'isanayashiro.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
